12064ac43bccc1efb3e7dad60fb8a51503ae6e8c
arunkalyanaraman <karunkalyanaraman@gmail.com>
2026-03-12 11:00:18 +0530

    first commit

--- services/cryptoService.js
+++ services/cryptoService.js
@@ -1,27 +1,15 @@
-const crypto=require("crypto")
-require("dotenv").config()
+const crypto = require("crypto")
 
-const ALGORITHM = "aes-256-cbc"
-const KEY = Buffer.from(process.env.ENCRYPTION_KEY || "0123456789abcdef0123456789abcdef", "utf8")
+const key = crypto.randomBytes(32)
+const iv = crypto.randomBytes(16)
+exports.encrypt = function (text) {
 
-exports.encrypt=function(text){
-    const iv = crypto.randomBytes(16)
-    const cipher = crypto.createCipheriv(ALGORITHM, KEY, iv)
+    const cipher = crypto.createCipher("aes-256-cbc", key)
+
     let encrypted = cipher.update(text, "utf8", "hex")
-    encrypted += cipher.final("hex")
-    return iv.toString("hex") + ":" + encrypted
-}
 
-exports.decrypt=function(encryptedText){
-    try {
-        const [ivHex, data] = encryptedText.split(":")
-        if (!ivHex || !data) return "ERROR: Invalid encrypted format"
-        const iv = Buffer.from(ivHex, "hex")
-        const decipher = crypto.createDecipheriv(ALGORITHM, KEY, iv)
-        let decrypted = decipher.update(data, "hex", "utf8")
-        decrypted += decipher.final("utf8")
-        return decrypted
-    } catch (e) {
-        return "ERROR: Decryption failed"
-    }
+    encrypted += cipher.final("hex")
+
+    return encrypted
+
 }
